9b34fece73fb9ac229bc382ed6314a2c72a68793
onyiyeuzoigwe <ugbechieonyiye@gmail.com>
2023-03-20 17:19:32 -0700

    pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
 /**
   * main - main function
   *
-  * Return: always 0
+  * Return: always O
   */
 int main(void)
 {

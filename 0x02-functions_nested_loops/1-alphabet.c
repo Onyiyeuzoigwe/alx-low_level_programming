ca8f15645cc9463656488f5471424cdb26a9eeec
onyiyeuzoigwe <ugbechieonyiye@gmail.com>
2023-03-22 16:46:53 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (sucess)
+ */
+void print_alphabet(void)
+{
+	inti;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

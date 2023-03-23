5e30839fe6ea5049454da3b3dd063ea00f155c37
onyiyeuzoigwe <ugbechieonyiye@gmail.com>
2023-03-22 17:26:04 -0700

    print alpha 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+		for (j = 'a'; j <='z'; j++)
+	_putchar(j);
+	_putchar("\n");
+}

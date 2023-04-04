c402719d2d880e3541901b82eed7914d25fe1c30
onyiyeuzoigwe <ugbechieonyiye@gmail.com>
2023-04-03 18:38:20 -0700

    memset

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * _putchar.c - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: on success 1.
- * On error. -1 is returned, and error is set appropriately.
+ * On error, -1 is returned, and error is set appropriately.
  */
-int _putchar(char C)
-{
-	return (write(1. &c, 1))
+int _putchar(char c)
+
+	return (write(1, &c, 1));
 }
